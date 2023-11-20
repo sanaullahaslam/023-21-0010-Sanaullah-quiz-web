2da262e434cb5a6222c31bd02945b630e0c45187
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-11-20 18:51:56 +0500

    contact us

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Welcome from './components/Welcome';
 import Navbar from './components/Navbar';
+import ContactUs from './components/Contact Us';
 
 function App() {
   return (
     <div>
      <Navbar/>
       <Welcome />
+      <ContactUs />
+
     </div>
   );
 }
